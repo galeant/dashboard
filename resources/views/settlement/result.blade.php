f79b1b549850699e29a42e18c7767bf9f9839a52
ilham <pigijopigijo@gmail.com>
2018-10-26 10:46:12 +0700

    ilangin button di settelement

--- resources/views/settlement/result.blade.php
+++ resources/views/settlement/result.blade.php
@@ -102,8 +102,6 @@
                                             @if($set->bank_account_number != null)
                                                 @if($set->status == 1)
                                                 <a id="paid" class="btn bg-deep-purple btn-block waves-effect" state="status_pay" data-id="{{$set->id}}">Pay</a>
-                                                @else
-                                                <a href="#" class="btn bg-green btn-block waves-effect" state="status_proceed">Proceed</a>
                                                 @endif
                                             @else
                                                 <a  href="#" class="btn bg-red" data-id="{{$set->id}}" data-toggle="modal" data-target="#myModal">
